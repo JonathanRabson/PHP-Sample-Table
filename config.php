b3344dd8075a89dc446bba7ebb9d6627a83edc8a
JonathanRabson <jonaanoj@aol.com>
2014-08-18 15:51:06 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+	//config variables; edit as needed.
+	class config {
+		public $hostname = 'localhost';
+		public $username = 'username';
+		public $password = 'password';
+		public $db = 'inventory';
+	}
+?>
